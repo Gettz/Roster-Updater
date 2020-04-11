dc4e77a55dbe320c0527d2f77814a02ae2381e44
Gettz <49850415+Gettz@users.noreply.github.com>
2020-04-11 03:08:22 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import json
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 
-version = '0.3'
+version = '0.4'
 
 #---- Sunwell Login Code
 
@@ -26,6 +26,7 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
+template = client.open("gear_template").sheet1
 sheet = client.open("Requiem_Roster_Gear")
 
 
@@ -44,7 +45,6 @@ def parse(ign):
         try:
             split3 = split2[3].split('=')
             if int(split3[1]) in blacklist:
-                print("skipped item ID: " + str(split2[3]))
                 pass
             else:
                 gearlist.append(split2[3])
@@ -95,50 +95,60 @@ def rostercheck():
         i += 1
 
     for name in names:
-        i = 1
-        x = 1
-        y = 1
+        i = 0
 
         worksheet = sheet.add_worksheet(title=name, rows="25", cols="5")
         parsed = parse(name)
         char = parsed[0]
         dirty = parsed[1]
 
+        itemlist = []
+        enchantlist = []
+        gemlist1 = []
+        gemlist2 = []
+        gemlist3 = []
+        gemdict = [gemlist1, gemlist2, gemlist3]
+
         for item in char:
             cleaned = gearcheck(item)
             link = "=HYPERLINK({},{})".format('"' + item + '"', '"' + cleaned + '"')
-            worksheet.update('A' + str(i), link, value_input_option='USER_ENTERED')
-            i += 1
-            time.sleep(1)
+            itemlist.append([link])
+
+        worksheet.update('A2', itemlist, value_input_option='USER_ENTERED')
 
         for item in dirty:
             if "ench" in item:
                 loc = item.find("ench")
                 stats = enchant(item[loc + 5:loc + 9])
-                if stats.startswith("+ 4" or "+ 6" or "+ 8"):
-                    pass
+                if stats.startswith("+4" or "+6" or "+5" or "+8"):
+                    enchantlist.append([])
                 else:
-                    worksheet.update('B' + str(x), stats)
+                    enchantlist.append([stats])
             else:
-                pass
+                enchantlist.append([])
 
-            x += 1
-            time.sleep(1)
+        worksheet.update('B2', enchantlist)
 
         for item in dirty:
-            print("dirty item" + item)
+            z = 0
             if "gems" in item:
-                z = 0
                 split = item[5:].split("&")
                 split2 = split[0].split(":")
-                for gem in split2:
-                    stats = gems(gem)
-                    worksheet.update(chr(ord('C') + z) + str(y), stats)
+                while z <= 2:
+                    try:
+                        stats = gems(split2[z])
+                        gemdict[z].append([stats])
+                    except IndexError:
+                        gemdict[z].append([])
                     z += 1
             else:
-                pass
-            y += 1
-            time.sleep(1)
+                gemdict[0].append([])
+                gemdict[1].append([])
+                gemdict[2].append([])
 
+        for x in gemdict:
+            worksheet.update(chr(ord('C') + i) + str(2), gemdict[i])
+            i += 1
 
 rostercheck()
+
